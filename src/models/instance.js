b545314f4903f80b9806b96b01bf566b177daa0d
Singh <siksingh@informatica.com>
2020-03-29 22:21:55 +0530

    added getLogs API to fetch catalina.out and node.log

--- src/models/instance.js
+++ src/models/instance.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema= mongoose.Schema;
-
+const logger=require('../../logger.js')
 
 var instanceSchema=new Schema({
     ihome: {
@@ -12,17 +12,34 @@ var instanceSchema=new Schema({
         required:true,
     },
     port:  {
-        type:String,
+        type:Number,
         required:true,
     },
     version:  {
         type:String,
         default:'0.0.0'
     },
+    sshPort:{
+      type:Number,
+      default:22
+    },
+    linuxUser:{
+      type:'String',
+
+    },
+    linuxPassword:{
+      type:'String'
+    },
+    logDirectory:{
+      type:'String'
+    }
 },
 {
  timestamps:true
 })
+
+
+
 
 var instance = mongoose.model('instances',instanceSchema)
 
--- src/routers/instance.js
+++ src/routers/instance.js
@@ -1,6 +1,8 @@
 const app=require('express')
 const instanceModel = require('../models/instance')
 const logger=require('../../logger')
+node_ssh = require('node-ssh')
+ssh = new node_ssh()
 
 const router=app.Router();
 
@@ -19,7 +21,49 @@ router.get('/' , async (req,res) => {
 })
 
 
+router.get('/getLogs/:id/:logType/:len', async (req,res) => {
+  const _id=req.params.id
+  const logType=req.params.logType
+  var len=req.params.len;
+  len=len>300?300:len
+  const instance = await instanceModel.findById({_id})
+  logger.log('info','fetching Logs from instance:'+instance.host+'  Directory: '+instance.logDirectory)
+  var config={
+    host: instance.host,
+    username: instance.linuxUser,
+    password: instance.linuxPassword
+};
+if(logType == 'catalina')
+ var CMD='tail -'+len+'  '+instance.logDirectory+'/catalina.out';
+else if(logType == 'node')
+ var CMD='tail -'+len+' '+ instance.logDirectory+'/node.log';
+else
+ console.log('here')
 
+try{
+
+ssh.connect(config).then(function(){
+  ssh.execCommand(CMD).then(function(result){
+    if(result.stdout)
+     res.send(result.stdout)
+    else
+     {logger.log('error',result.stderr);res.send('Something went wrong')}
+  })
+})
+
+}catch(e)
+{
+  res.status(400).send('Something went wrong, please refresh.')
+  logger.log('error','Something went wrong while fetching logs from server')
+}
+
+
+
+
+
+
+
+})
 
 router.post('/',  async (req,res) => {
 
@@ -36,6 +80,7 @@ router.post('/',  async (req,res) => {
 
  res.status(200).send(instance)
  logger.log('info','Saved instance succesfully '+instance)
+
 })
 
 
