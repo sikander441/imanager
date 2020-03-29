f09f757d821066d12f9674d7ecfa139bf0de8862
Singh <siksingh@informatica.com>
2020-03-30 02:37:15 +0530

    added basic functionality to get all domain info and issue the startup command

--- src/models/instance.js
+++ src/models/instance.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const Schema= mongoose.Schema;
 const logger=require('../../logger.js')
+node_ssh = require('node-ssh')
+var parseString = require('xml2js').parseString;
 
 var instanceSchema=new Schema({
     ihome: {
@@ -9,11 +11,11 @@ var instanceSchema=new Schema({
     },
     host:  {
         type:String,
-        required:true,
+        required:true
     },
     port:  {
         type:Number,
-        required:true,
+        default:0000
     },
     version:  {
         type:String,
@@ -25,13 +27,35 @@ var instanceSchema=new Schema({
     },
     linuxUser:{
       type:'String',
-
+      required:true
     },
     linuxPassword:{
-      type:'String'
+      type:'String',
+      required:true
     },
     logDirectory:{
-      type:'String'
+      type:'String',
+      default:'x/x/x/x'
+    },
+    domainName:{
+      type:'String',
+      default:'xxxxx'
+    },
+    instanceUser:{
+      type:'String',
+      required:true
+    },
+    instancePassword:{
+      type:'String',
+      required:true
+    },
+    status:{
+      type:'String',
+      default:'Down'
+    },
+    isDocker:{
+      type:Boolean,
+      default:false
     }
 },
 {
@@ -39,7 +63,163 @@ var instanceSchema=new Schema({
 })
 
 
+instanceSchema.methods.updateSystemLogDirectory = async function()
+{
 
+  var instance = this
+  var config={
+    host: instance.host,
+    username: instance.linuxUser,
+    password: instance.linuxPassword,
+    port: instance.sshPort
+ };
+
+ var CMD=this.ihome + '/server/bin/infacmd.sh getSystemLogDirectory';
+ try{
+
+ ssh.connect(config).then(function(){
+   ssh.execCommand(CMD).then(async function(result){
+     instance.logDirectory=result.stdout.split('\n')[0]
+     await instance.save()
+     // ssh.dispose()
+   }).catch((err)=>{console.log(err);logger.log('error',err)})
+ }).catch((err)=>{console.log(err);logger.log('error',err)})
+
+ }catch(e)
+ {
+ logger.log('error','Something went wrong, please refresh'+e)
+
+ }
+}
+instanceSchema.methods.bringUp = async function(){
+  var instance = this
+  var config={
+    host: instance.host,
+    username: instance.linuxUser,
+    password: instance.linuxPassword,
+    port: instance.sshPort
+ };
+
+ var CMD=this.ihome + '/tomcat/bin/infaservice.sh startup';
+ try{
+
+ ssh.connect(config).then(function(){
+   ssh.execCommand(CMD).then(async function(result){
+     logger.log('info',result.stdout || result.stderr)
+     // ssh.dispose();
+   }).catch((err)=>logger.log('error',err))
+ }).catch((err)=>logger.log('error',err))
+
+ }catch(e)
+ {
+logger.log('error','Something went wrong, please refresh'+e)
+ }
+
+}
+instanceSchema.methods.getVersion = async function(){
+  var instance = this
+  var config={
+    host: instance.host,
+    username: instance.linuxUser,
+    password: instance.linuxPassword,
+    port: instance.sshPort
+ };
+
+ var CMD=this.ihome + '/server/bin/infacmd.sh version | grep -i version';
+ try{
+
+ ssh.connect(config).then(function(){
+   ssh.execCommand(CMD).then(async function(result){
+    instance.version=result.stdout.split(':')[1]
+    await instance.save();
+    // ssh.dispose()
+   }).catch((err)=>logger.log('error',err))
+ }).catch((err)=>logger.log('error',err))
+
+ }catch(e)
+ {
+logger.log('error','Something went wrong, please refresh'+e)
+ }
+
+}
+instanceSchema.methods.updateStatus = async function(){
+  var instance = this
+  var config={
+    host: instance.host,
+    username: instance.linuxUser,
+    password: instance.linuxPassword,
+    port: instance.sshPort
+ };
+
+ var CMD=this.ihome + '/server/bin/infacmd.sh ping -dn '+this.domainName+'|grep \"was successfully pinged\"';
+ try{
+
+ ssh.connect(config).then(function(){
+   ssh.execCommand(CMD).then(async function(result){
+     if(result.stderr)
+       logger.log('error',result.stderr)
+     else if(result.stdout){
+      logger.log('info','status is set to UP')
+      instance.status="UP"
+    }
+     else{
+       logger.log('info','status is set to down for instance: '+instance)
+      instance.status="DOWN"
+    }
+    await instance.save();
+    // ssh.dispose()
+   }).catch((err)=>logger.log('error',err))
+ }).catch((err)=>logger.log('error',err))
+
+ }catch(e)
+ {
+logger.log('error','Something went wrong, please refresh'+e)
+ }
+
+}
+
+instanceSchema.methods.updateDomainInfo = async function(){
+
+ var instance = this
+ var config={
+   host: instance.host,
+   username: instance.linuxUser,
+   password: instance.linuxPassword,
+   port: instance.sshPort
+};
+
+var CMD='cat '+instance.ihome+'/domains.infa'
+try{
+
+ssh.connect(config).then(function(){
+  ssh.execCommand(CMD).then(function(result){
+    if(result.stdout){
+
+    xmlData=result.stdout
+    parseString(xmlData, async function (err, result) {
+      if(!instance.isDocker)
+      {
+        instance.host=result.Portals.vector[0].address[0].host[0]
+        instance.port=result.Portals.vector[0].address[0].port[0]
+      }
+      instance.domainName=result.Portals.vector[0].domainName[0]
+      await instance.save();
+   });
+
+   }
+    else
+     {logger.log('error','Something went wrong, please refresh'+result.stderr)}
+     // ssh.dispose()
+  }).catch( (err)=>{logger.log('error',err)})
+}).catch((err)=>logger.log('error',err))
+
+}catch(e)
+{
+logger.log('error','Something went wrong, please refresh'+e)
+}
+
+
+}
 
 var instance = mongoose.model('instances',instanceSchema)
 
--- src/routers/instance.js
+++ src/routers/instance.js
@@ -6,8 +6,44 @@ ssh = new node_ssh()
 
 const router=app.Router();
 
+router.get('/startup/:id',async (req,res) =>{
 
+  const _id=req.params.id
+  try{
+  var instance = await instanceModel.findById({_id})
+  }catch(e){
+  logger.log('error','No such instance found',e)
+  return res.send('No such instance found,please check object id')
+  }
+  if(!instance){
+   res.send('Error no instance found')
+   return -1;
+ }
+  await instance.bringUp();
+  res.send('check the logs, command ran')
+})
+
+
+router.get('/refreshDomain/:id',async (req,res)=> {
+  const _id=req.params.id
+  try{
+  var instance = await instanceModel.findById({_id})
+  }catch(e){
+  logger.log('error','No such instance found',e)
+  return res.send('No such instance found,please check object id')
+  }
+  if(!instance){
+   res.send('Error no instance found')
+   return -1;
+ }
+
+  await instance.updateSystemLogDirectory();
+  await instance.updateStatus();
+res.send(instance)
+
+})
 router.get('/' , async (req,res) => {
+
   try{
     var instances = await instanceModel.find({})
   }
@@ -21,17 +57,47 @@ router.get('/' , async (req,res) => {
 })
 
 
+
+router.get('/isUp/:id',async (req,res) => {
+  const _id=req.params.id
+  try{
+  var instance = await instanceModel.findById({_id})
+  }catch(e){
+  logger.log('error','No such instance found',e)
+  return res.send('No such instance found,please check object id')
+  }
+  if(!instance){
+   res.send('Error no instance found')
+   return -1;
+ }
+  await instance.updateStatus();
+  res.send(instance.status);
+
+})
+
+
 router.get('/getLogs/:id/:logType/:len?', async (req,res) => {
   const _id=req.params.id
   const logType=req.params.logType
   var len=req.params.len || 50;
-  len=len>300?300:len
-  const instance = await instanceModel.findById({_id})
+  len=len>800?800:len
+  try{
+  var instance = await instanceModel.findById({_id})
+}catch(e){
+  logger.log('error','No such instance found',e)
+  return res.send('No such instance found,please check object id')
+}
+
+ if(!instance){
+  res.send('Error no instance found')
+  return -1;
+}
   logger.log('info','fetching Logs from instance:'+instance.host+'  Directory: '+instance.logDirectory)
   var config={
     host: instance.host,
     username: instance.linuxUser,
-    password: instance.linuxPassword
+    password: instance.linuxPassword,
+    port: instance.sshPort
 };
 if(logType == 'catalina')
  var CMD='tail -'+len+'  '+instance.logDirectory+'/catalina.out';
@@ -50,18 +116,15 @@ ssh.connect(config).then(function(){
    }
     else
      {logger.log('error',result.stderr);res.send('Something went wrong')}
-  })
-})
+     ssh.dispose()
+  }).catch((err)=>logger.log('error',err))
+}).catch((err)=>logger.log('error',err))
 
 }catch(e)
 {
   res.status(400).send('Something went wrong, please refresh.')
   logger.log('error','Something went wrong while fetching logs from server')
 }
-
-
-
-
 
 
 
@@ -83,6 +146,10 @@ router.post('/',  async (req,res) => {
  res.status(200).send(instance)
  logger.log('info','Saved instance succesfully '+instance)
 
+
+ await instance.updateDomainInfo();
+ await instance.updateStatus();
+ await instance.getVersion();
 })
 
 
