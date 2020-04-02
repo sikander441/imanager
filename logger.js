003fc86adc1d4589baa469787dc695b24f8f5290
Singh <siksingh@informatica.com>
2020-04-02 18:31:29 +0530

    printing stack for error and just message for INFO

--- logger.js
+++ logger.js
@@ -3,12 +3,21 @@ const { format, transports } = winston;
 
 const files = new winston.transports.File({ filename: './logs/imanager.log' });
 winston.add(files);
+
+msg = (info)=>{
+  if(info.level=='error')
+   return `${info.timestamp} [${info.level.toUpperCase()}]: ${info.stack}`
+  else
+  return `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`
+}
+
+
 winston.configure({
   format: format.combine(
     format.timestamp({
          format: 'YYYY-MM-DD HH:mm:ss'
        }),
-      format.printf(info => `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`),
+      format.printf(info => msg(info)),
     ),
     transports: [
       new winston.transports.File({ filename: './logs/imanager.log' }),
