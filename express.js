05a7626045868604e91971959abf25b55af7a8a6
Singh <siksingh@informatica.com>
2020-04-10 18:03:46 +0530

    Added access logger morgan for express requsts

--- express.js
+++ express.js
@@ -1,7 +1,15 @@
 const express=require('express')
 const bodyParser = require("body-parser");
+var morgan = require('morgan')
+var fs = require('fs')
+var path = require('path')
+
+
 
 const app=express();
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, '/logs/access.log'), { flags: 'a' })
+app.use(morgan('short', { stream: accessLogStream }))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
