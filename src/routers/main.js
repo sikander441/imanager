185b394c94d539f6c0f655d1dcb6395e42913377
Singh <siksingh@informatica.com>
2020-06-12 18:13:58 +0530

    added route to get instances particular team

--- src/routers/main.js
+++ src/routers/main.js
@@ -6,6 +6,21 @@ router.get('/',(req,res) => {
   res.status(200).send('Main router')
 })
 
+router.get('/getInstances/:teamName',async (req,res) =>{
+  teamName = req.params.teamName
+  try{
+    let team = await teamModel.findOne({teamName})
+    console.log(team);
+    console.log(teamName)
+    team = await team.populate('instances').execPopulate();
+    res.status(200).send(team);
+  }catch(e){
+    logger.log('error',e)
+    res.status(400).send({status:'failed',message:"something went wrong "+e.message})
+  }
+ 
+})
+
 router.post('/RegisterTeam',async (req,res) => {
   try{
     const teamName=req.body.teamName
